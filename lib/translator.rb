55e987b82cca3b8227370075384838bcda24bf80
optimistic-writer-7178 <duot.jim@gmail.com>
2019-07-25 09:11:08 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,13 @@
-# require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library file
+  YAML.load_file file
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(path, western)
+  load_library(path)['get_emoticon'][western] or "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning path, japanese
+  load_library(path)['get_meaning'][japanese] or "Sorry, that emoticon was not found"
 end
