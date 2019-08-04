07e1fb9e1dbaacf22b3a5042ce7ca2ddf9a05804
Srikalyan Megati <kalmegati@gmail.com>
2019-08-04 20:44:25 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,32 @@
-# require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+# in get_meaning, japanese emoticons point to written meanings
+# in get_emoticon, english emoticons point to japanese emoticons
+
+
+def load_library(file)
+  runner_speak = YAML.load_file(file)
+  feelings = runner_speak.keys
+  paired_emojis = runner_speak.values
+  japanemote = []
+  englicons = []
+  paired_emojis.each {|pair| japanemote.push(pair[1])}
+  paired_emojis.each {|pair| englicons.push(pair[0])}
+  emotionary = {'get_meaning': {}, 'get_emoticon': {}}
+  #print feelings
+  #print "\n"
+  #print japanemote
+  #print "\n"
+  #print englicons
+  
+  japanemote.each { |owo| emotionary['get_meaning'][owo] = ""}
+  
+  print emotionary
 end
+
+load_library("lib/emoticons.yml")
+
+
 
 def get_japanese_emoticon
   # code goes here
