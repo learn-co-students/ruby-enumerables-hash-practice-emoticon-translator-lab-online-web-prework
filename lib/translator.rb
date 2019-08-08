1807189f0644d9938e0c36e88183d3379e81182a
Christina Gonzalez <cmgamway@gmail.com>
2019-08-08 22:50:04 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,72 @@
-# require modules here
-
+require "emoticons.yml"
 def load_library
-  # code goes here
+
 end
 
 def get_japanese_emoticon
-  # code goes here
+when "when an element from get_emoticon is used it will print the corresponding element from get_meaning" do
+    :get_emoticon => {
+      :angel => ["O:)"]
+      :angry => [">:("]
+      :bored => [":O"]
+      :confused => ["%)"]
+      :embarrased => [":$"]
+      :fish => ["><>"]
+      :glasses => ["8D"]
+      :grinning => ["=D"]
+      :happy => [":)"]
+      :kiss => [":*"]
+      :sad => [":'("]
+      :surprised => [":o"]
+      :wink => [";)"]
+    },
+    :get_meaning => {
+      :angel => ["☜(⌒▽⌒)☞"]
+      :angry => ["ヽ(ｏ`皿′ｏ)ﾉ"]
+      :bored => ["(ΘεΘ;)"]
+      :confused => ["(゜.゜)"]
+      :embarrased => ["(#^.^#)"]
+      :fish => [">゜))))彡"]
+      :glasses => ["(^0_0^)"]
+      :grinning => ["(￣ー￣)"]
+      :happy => ["(＾ｖ＾)"]
+      :kiss => ["(*^3^)/~☆"]
+      :sad => ["(Ｔ▽Ｔ)"]
+      :surprised => ["o_O"]
+      :wink => [ "(^_-)"]
+    }
 end
 
 def get_english_meaning
-  # code goes here
+when "when an element from get_meaning is used it will print the corresponding element from get_emoticon" do
+  :get_meaning => {
+    :angel => ["☜(⌒▽⌒)☞"]
+    :angry => ["ヽ(ｏ`皿′ｏ)ﾉ"]
+    :bored => ["(ΘεΘ;)"]
+    :confused => ["(゜.゜)"]
+    :embarrased => ["(#^.^#)"]
+    :fish => [">゜))))彡"]
+    :glasses => ["(^0_0^)"]
+    :grinning => ["(￣ー￣)"]
+    :happy => ["(＾ｖ＾)"]
+    :kiss => ["(*^3^)/~☆"]
+    :sad => ["(Ｔ▽Ｔ)"]
+    :surprised => ["o_O"]
+    :wink => [ "(^_-)"]
+  },
+  :get_emoticon => {
+    :angel => ["O:)"]
+    :angry => [">:("]
+    :bored => [":O"]
+    :confused => ["%)"]
+    :embarrased => [":$"]
+    :fish => ["><>"]
+    :glasses => ["8D"]
+    :grinning => ["=D"]
+    :happy => [":)"]
+    :kiss => [":*"]
+    :sad => [":'("]
+    :surprised => [":o"]
+    :wink => [";)"]
+  }
 end
