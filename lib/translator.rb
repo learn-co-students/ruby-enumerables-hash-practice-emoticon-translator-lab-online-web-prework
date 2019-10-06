6410829d4b5c40d125f164f14d4a6e95095254f3
Tariq Bailey <tariqbailey627@gmail.com>
2019-10-06 20:29:47 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,16 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(the_file)
+emot = {:get_meaning => {}, :get_emoticon => {} }
+emoji = YAML.load_file(the_file)
+emoji.each do |eng_word, array_emot|
+emot[:get_meaning][array_emot[1]] = eng_word
+emot[:get_emoticon][array_emot[0]] = array_emot[1] 
+#binding.pry
+  end
+  emot
 end
 
 def get_japanese_emoticon
