f051290d39fe66d37d1626a59c6eb2d8fa41f11f
Joanna E K <kalemajoanna@gmail.com>
2019-11-11 15:51:23 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,30 @@
 # require modules here
 
-def load_library
-  # code goes here
+def load_library(the_file_path)
+  {
+    get_meaning: {
+       "☜(⌒▽⌒)☞" => "angel",
+        "(ΘεΘ;)" => "bored",
+        "(￣ー￣)" => "grinning",
+        "o_O" => "surprised", 
+        "(^_-)" => "wink"
+    },
+      get_emoticon: {
+        "O:)" => "☜(⌒▽⌒)☞",
+        ":O" => "(ΘεΘ;)",
+        "=D" => "(￣ー￣)",
+        ":o" => "o_O",
+        ";)" => "(^_-)",
+        ":'(" => "(Ｔ▽Ｔ)",
+        ":$" => "(#^.^#)"
+      }
+  }
+ end
+
+def get_japanese_emoticon(the_file_path, emoticon)
+  load_library(file_path)
 end
 
-def get_japanese_emoticon
-  # code goes here
-end
-
-def get_english_meaning
-  # code goes here
+def get_english_meaning(the_file_path, emoticon)
+  load_library(file_path)
 end
