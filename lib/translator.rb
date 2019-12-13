8a16399b0be1f57ad2b578147ccc8cde360a9260
laura hatch <laura.hatch.lh@gmail.com>
2019-12-12 20:10:26 -0600

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,24 @@
 # require modules here
+require 'pry'
+require "yaml"
+require 'pp'
 
-def load_library
-  # code goes here
+def load
+  _library(emojo_file)
+  data = YAML.load_file(emojo_file)
+    
+  emojo_hash = {
+          'get_word' => {},
+          'get_emojo' => {}
+  }
+
+  data.each do|key,value|
+  emojo_hash['get_emojo'][value[0]] = data[key][1]
+  emojo_hash['get_word'][value[1]] = key
+    end
+    emojo_hash
 end
+pp emojo_hash
 
 def get_japanese_emoticon
   # code goes here
