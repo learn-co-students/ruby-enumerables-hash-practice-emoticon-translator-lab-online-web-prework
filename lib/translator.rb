203fd572aaadec39b561ae0cbe748de83fee0c33
Dan Foley <dan@dan-foley.com>
2019-12-19 17:49:17 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,29 +10,31 @@ def load_library(path)
   new_hash = {}
   lib.each do |meaning, eng_jp|
       # checks for the key existence
-      if !new_hash["get_meaning"]
+      if !new_hash[:get_meaning]
         # if doesn't exist make it an empty hash
-        new_hash["get_meaning"] = {}
+        new_hash[:get_meaning] = {}
       end
       # checks for the key/value pair existence
-      if !new_hash["get_meaning"][eng_jp[1]]
+      if !new_hash[:get_meaning][eng_jp[1]]
         # if doesn't exist create the appropriate k/v pair
-        new_hash["get_meaning"][eng_jp[1]] = meaning
+        new_hash[:get_meaning][eng_jp[1]] = meaning
       end
-      if !new_hash["get_emoticon"]
-        new_hash["get_emoticon"] = {}
+      if !new_hash[:get_emoticon]
+        new_hash[:get_emoticon] = {}
       end
-      if !new_hash["get_emoticon"][eng_jp[0]]
-        new_hash["get_emoticon"][eng_jp[0]] = eng_jp[1]
+      if !new_hash[:get_emoticon][eng_jp[0]]
+        new_hash[:get_emoticon][eng_jp[0]] = eng_jp[1]
       end
-      # binding.pry
   end
   new_hash
 end
 
 def get_japanese_emoticon(path, emoticon)
-  # code goes here
-
+  # when provided with an eng_emoticon
+  # it returns the Japanese equivalent
+  
+  emoticon_hash = load_library(path)
+  binding.pry
 end
 
 def get_english_meaning
