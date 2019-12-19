2b89f4c31e20bab45bb76476ddef55b024e32cc1
Dan Foley <dan@dan-foley.com>
2019-12-19 00:46:15 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,18 @@
 # require modules here
+require "yaml"
+require "pry"
 
-def load_library
+
+def load_library(path)
   # code goes here
+  lib = YAML.load_file(path)
+  # binding.pry
 end
 
 def get_japanese_emoticon
   # code goes here
+  lib = load_library(path)
+  binding.pry
 end
 
 def get_english_meaning
